e83cd981a2e8612b53f5cb69061ce1aaf2bd0b1a
lee-maru <uffortune@gmail.com>
2021-09-22 15:03:32 +0900

    Solution78진행중

--- src/week06/Solution78.java
+++ src/week06/Solution78.java
@@ -3,11 +3,24 @@ package week06;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Stream;
 
 public class Solution78 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int Y = Integer.parseInt(br.readLine());
+        int X = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[Y][X];
+        int[][] copyMatrix = new int[Y][X];
 
+        for (int i = 0; i < Y ; i++) {
+            String[] s = br.readLine().split("");
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = Integer.parseInt(s[j]);
+            }
+        }
+
+        for (int i = 0; i < Y; i++) {
+
+        }
     }
 }
