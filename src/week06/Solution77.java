a6ef4d7d4437c543e6bc41d5e63e2d11d0bc406d
lee-maru <uffortune@gmail.com>
2021-09-22 14:24:40 +0900

    Solution77완료

--- src/week06/Solution77.java
+++ src/week06/Solution77.java
@@ -1,53 +1,40 @@
 package week06;
-
 // https://www.acmicpc.net/problem/1946
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution77 {
-    public static class Apply implements Comparable<Apply>{
-        int resumeScore;
-        int interviewScore;
 
-        public Apply(int resumeScore, int interviewScore){
-            this.resumeScore = resumeScore;
-            this.interviewScore = interviewScore;
-        }
-
-        @Override // 1차 점수를 통해 지원자 분들의 정렬을 진행
-        public int compareTo(Apply otherApplicant){
-            if(this.resumeScore >= otherApplicant.resumeScore){
-                return 1; // 서류 점수로 오름차순 진행
-            }else{
-                return -1; // 점수가 낮거나 같으면 기본으로 정렬
-            }
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         int[] result = new int[T];
         for (int i = 0; i < T; i++) {
-            int N = sc.nextInt();
-            int cnt = 1;
-            List<Apply> list = new ArrayList<>();
-            for (int j = 0; j < N; j++) {
-                list.add(new Apply(sc.nextInt(), sc.nextInt()));
+
+            int N = Integer.parseInt(br.readLine()) + 1;
+            int[] list = new int[N];
+            for (int j = 0; j < N-1; j++) {
+
+                String s = br.readLine();
+                String[] s1 = s.split(" ");
+                list[Integer.parseInt(s1[0])] = Integer.parseInt(s1[1]);
             }
-
-            Collections.sort(list);
-
-            for (int j = 0; j < list.size(); j++) {
-                if(j == list.size()-1){
-                    break;
-                }
-                if(list.get(j).interviewScore > list.get(j+1).interviewScore){
+            int cnt = 1;
+            int start = list[1];
+            for (int j = 1; j < list.length; j++) {
+                if(start > list[j]){
+                    start = list[j];
                     cnt++;
                 }
+
             }
             result[i] = cnt;
         }
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+
+        for (int j : result) {
+            System.out.println(j);
         }
     }
 }
---
+++ src/week06/Solution78.java
@@ -0,0 +1,13 @@
+package week06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Solution78 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
