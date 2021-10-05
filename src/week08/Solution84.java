ecec345aaa5adcd8ac9025ddd25475f446853ad4
lee-maru <uffortune@gmail.com>
2021-10-05 21:41:16 +0900

    Solution84 완료

--- src/week08/Solution84.java
+++ src/week08/Solution84.java
@@ -7,11 +7,13 @@ public class Solution84 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
         List<String> arr = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            arr.add(sc.nextLine());
+            String contain = sc.next();
+
+            if(!arr.contains(contain))
+                arr.add(contain);
         }
-        arr.stream().sorted(Comparator.comparing(String::length)).forEach(System.out::println);
+        arr.stream().sorted(Comparator.comparing(String::length).thenComparing(String::compareTo)).forEach(System.out::println);
     }
 }
