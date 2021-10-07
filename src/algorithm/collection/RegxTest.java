8d5407a4454f7a12ee36423705cdfb2c3aab9ab9
lee-maru <uffortune@gmail.com>
2021-10-07 21:42:46 +0900

    정규식 스티디 추가

--- src/algorithm/collection/RegxTest.java
+++ src/algorithm/collection/RegxTest.java
@@ -1,10 +1,19 @@
 package algorithm.collection;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegxTest {
     public static void main(String[] args) {
-        String str = "/backoffice/user/investorList";
-        String str2 = "/backoffice/app/v1/faq/getFrequentList";
-        System.out.println(str.matches("app/v1"));
-        System.out.println(str2.matches("(.*)app/v1(.*)"));
+        String str4 = "http://localhost:8480/backoffice/";
+        String str5 = "https://localhost:8480/backoffice/inquiry/inquiryList";
+        String matxh = "(https?:\\/\\/)?(www\\.)?(localhost:8480)(\\/backoffice)\\/*([a-zA-Z\\/]*)";
+        Pattern pattern = Pattern.compile(matxh);
+        Matcher m = pattern.matcher(str4);
+        String rr = "(https?:\\/\\/)?(www\\.)?(localhost:8480)(\\/backoffice)\\/*([a-zA-Z\\/]*)";
+        System.out.println(matxh);
+        System.out.println(Pattern.matches(rr,str5));
+        System.out.println(m.find());
+        System.out.println("5: " + m.group(5));
     }
 }
